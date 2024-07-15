8fdee0d9ac9f200fe6891d3427c42cac1c2c81ce
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2024-07-15 19:13:45 -0400

    examen m3 2019 v2

--- paint.js
+++ paint.js
@@ -14,6 +14,13 @@ function menu(matriz){
                 break
             case "reemplazar color":
                 reemplazo(matriz)
+                break
+            case "cruz":
+                cruz(matriz)
+                break
+            case "diagonal":
+                diagonal(matriz)
+                break 
             case "salir":
                 rl.close()
                 break
@@ -23,21 +30,20 @@ function menu(matriz){
                 break;
         }
     })
-
-
 }
 function interfaz(){
     console.log("--------Menu---------")
-    console.log("P: Herramienta de punto")
-    console.log("C: Definir color de brocha")
-    console.log("F: definir el color de fonsdo actual")
-    console.log("pintar: Pinta un punto con la herramienta y con el color de brocha actuales.")
-    console.log("reemplazar color: Pide el color a reemplazar y luego el color por el que se reemplaza.")
-    console.log("rotar: Rota el lienzo en 90 grados.")
-    console.log("diagonal: Solicita un punto y dibuja una diagonal.")
-    console.log("cruz: Solicita un punto y dibuja una cruz de extremo a extremo.")
-    console.log("histograma: Muestra el histograma de colores.")
-    console.log("salir:")
+    console.log("P                 |Herramienta de punto")
+    console.log("C                 |Definir color de brocha")
+    console.log("F                 |definir el color de fonsdo actual")
+    console.log("pintar            |Pinta un punto con la herramienta y con el color de brocha actuales.")
+    console.log("reemplazar color  |Pide el color a reemplazar y luego el color por el que se reemplaza.")
+    console.log("rotar             |Rota el lienzo en 90 grados.")
+    console.log("diagonal          |Solicita un punto y dibuja una diagonal.")
+    console.log("cruz              |Solicita un punto y dibuja una cruz de extremo a extremo.")
+    console.log("histograma        |Muestra el histograma de colores.")
+    console.log("salir")
+    console.log("-------------------------------")
 }
 
 function desplegar_lienzo(largo,ancho){
@@ -66,9 +72,54 @@ function pintar_punto(estado){
     })
 }
 
-function reemplazo(estado, colorOriginal, colorNuevo){
+function reemplazo(estado){
+    rl.question("Igrese el color original que desea cambiar: ", (colorOriginal) => {
+        rl.question("Ingrese el color nuevo: ", (colorNuevo) => {
+            for(let i = 0; i < estado.length ; i++){
+                for(let j = 0; j < estado[i].length ; j++){
+                    if (estado[i][j] === colorOriginal){
+                        estado[i][j] = colorNuevo;
+                        console.log("Color reemplazado")
+                    }
+                }
+            }
+            lienzo_pantalla(estado)
+            menu(estado)
+        })
+    })
 }
 
+function rotar(estado){
+
+}
+
+function diagonal(estado){
+    rl.question("Ingrese la coordenanda x del centro: ", (x) => {
+        rl.question("Ingrese la coordenada y del centro: ", (y) =>{
+            for(let i = 1;  i < estado.length; i++){
+                estado[i][i+1] = "D"
+            }
+            lienzo_pantalla(estado)
+            menu (estado)
+        })
+    })
+}
+
+function cruz(estado){ 
+    rl.question("Ingrese la coordenada x del centro: ", (x) => {
+        rl.question("Ingrese la coordenada y del centro: ", (y) => {
+            for (let i = 0; i < estado.length; i++){
+                estado[i][y-1] = "C"
+            }
+            for(let j = 0; j < estado.length; j++){
+                estado[x-1][j] = "C"
+            }
+            lienzo_pantalla(estado)
+            menu(estado)
+        })
+
+    })
+}
 
 rl.question('Ingrese el ancho del tablero:  ', (ancho) => {
     rl.question('Ingrese el largo del tablero ', (largo) => {
